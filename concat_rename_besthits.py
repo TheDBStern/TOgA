ee9d117fdb1ac878a6872beddc14f79347af0470
David Stern <dbstern@login4.cm.cluster>
2017-02-25 16:02:07 -0500

    works now

--- concat_rename_besthits.py
+++ concat_rename_besthits.py
@@ -7,15 +7,21 @@ from Bio import SeqIO
 
 species, DIR = sys.argv[1:]
 if DIR[-1] != "/": DIR += "/"
-output = open(species, 'w')
-
+output = open(species+'.fasta', 'w')
+f_count = 0
+t_count = 0
 for fasta_file in os.listdir(DIR):
 	if fasta_file.endswith('.besthit.fasta'):
-		count = 0:
-		cluster = fasta_file.split('\.')[0]
-		with SeqIO.read(DIR+fasta_file, 'fasta') as f:
-			for record in f:
-				record.id = cluster+'_'+count
-				record.description = cluster+'_'+count
-				count +=1
-				SeqIO.write(record, 'fasta')
+		print 'Adding '+fasta_file
+		f_count +=1
+		counter = 0
+		cluster = fasta_file.split('.')[0]
+		fasta = SeqIO.parse(DIR+fasta_file, 'fasta')
+		for record in fasta:
+			t_count +=1
+			record.id = cluster+'_'+str(counter)
+			record.description = cluster+'_'+str(counter)
+			counter +=1
+			SeqIO.write(record, output, 'fasta')
+
+print 'Processed %d assemblies with a total of %d transcripts' % (f_count, t_count)
